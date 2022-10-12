83d18d342578797c3aac240a60e8bf816499e6f5
Kacper <baginski.kacper12@gmail.com>
2022-10-12 19:28:52 +0200

    .

--- JavaScript/6-obiekt-json/script.js
+++ JavaScript/6-obiekt-json/script.js
@@ -1,7 +1,10 @@
-{
-    "pracownicy":[
-        {"firstName": "Krystian", "lastName": "Dziopa"}, 
-        {"firstName": "Anna", "lastName": "Szapiel"},
-        {"firstName": "Piotr", "lastName": "Żmuda"}
-    ]
-}
+let jsonPracownicy = { "pracownicy": [{ "firstName": "Krystian", "lastName": "Dziopa" }, { "firstName": "Anna", "lastName": "Szapiel" }, { "firstName": "Piotr", "lastName": "Żmuda" }] };
+
+console.log(jsonPracownicy);
+
+jsonPracownicy.pracownicy.forEach(function funkcja(element, index) {
+    console.log("Imie: " + element.firstName)
+    console.log("Nazwisko " + element.lastName);
+    console.log("Index: " + index);
+
+});
